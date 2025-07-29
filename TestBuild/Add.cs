76b989679c6910f51ec9f34261220aa53953c9f5
vaibhav-jadhav96 <vaibhavjadhav3489>
2025-07-29 08:05:15 +0530

    Add project files.

---
+++ TestBuild/Add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBuild
+{
+    internal class Add
+    {
+        public void Addition()
+        {
+            int a=12, b=23;
+            int c=a+b;
+            Console.WriteLine("Addition " + c);
+        }
+    }
+}
